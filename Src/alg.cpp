bb6d9b7e324b22588d7f397dfa3be9af918f95da
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-03-25 00:16:43 +0300

    Update and rename src/alg.cpp to Src/alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   return 0;
+  int count = 0;
+  int i;
+  int j;
+  for (i=0; i<len; i++) {
+    for (j=i+1; j<len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count += 1;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
