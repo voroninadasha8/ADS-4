f94016a545ab617ff184153d27d9351e6732ab07
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-03-30 22:28:13 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -2,45 +2,45 @@
 
 int countPairs1(int *arr, int len, int value) {
   return 0;
-  int count = 0;
+  int count1 = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        count += 1;
+        count1 += 1;
       }
     }
   }
-  return count;
+  return count1;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
-  int count = 0;
+  int count2 = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = len - 1; i < j; j--) {
       if (arr[i] + arr[j] == value) {
-        count += 1;
+        count2 += 1;
       }
     }
   }
-  return count;
+  return count2;
 }
 int countPairs3(int *arr, int len, int value) {
   return 0;
-  int count = 0;
+  int count3 = 0;
   for (int i = 0; i < len - 1; i++) {
     int left = i, right = len;
     while (left < right - 1) {
       int center = (left + right) / 2;
       if (arr[i] + arr[center] == value) {
-        count += 1;
+        count3 += 1;
         int newcenter = center + 1;
         while (arr[i] + arr[newcenter] == value && newcenter < right) {
-          count += 1;
+          count3 += 1;
           newcenter += 1;
         }
         newcenter = center - 1;
         while (arr[i] + arr[newcenter] == value && newcenter > left) {
-          count += 1;
+          count3 += 1;
           newcenter -= 1;
         }
         break;
@@ -52,5 +52,5 @@ int countPairs3(int *arr, int len, int value) {
       }
     }
   }
-  return count;
+  return count3;
 }
