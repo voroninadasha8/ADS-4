1b7fbf4bde713f708fadc5a47d0c26277d5f6bbb
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-04-01 11:06:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@ int countPairs1(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        found += 1;
+        count += 1;
       }
     }
   }
